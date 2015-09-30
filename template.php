21e87640ef128112f3c42fc0efcfea521d77f2ad
Evgeny Saltykov <es@zanami.ru>
2015-09-30 11:31:00 +1000

    menu block override theme

--- template.php
+++ template.php
@@ -52,7 +52,17 @@ function idest_menu_tree__menu_block($variables) {
  * Implements hook_menu_link__menu_block().
  * see Menu Block module docs https://www.drupal.org/node/1934824
  */
+/* -- Delete this line if you want to use this function
 
-function idest_menu_link__menu_block() {
-	
+function idest_menu_link__menu_block($variables) {
+	// default D7 code
+	$element = $variables ['element'];
+	  $sub_menu = '';
+
+	  if ($element ['#below']) {
+	    $sub_menu = drupal_render($element ['#below']);
+	  }
+	  $output = l($element ['#title'], $element ['#href'], $element ['#localized_options']);
+	  return '<li' . drupal_attributes($element ['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
+//*/
